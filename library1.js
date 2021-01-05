c8339a13464719b036686a7d6728967f413d87f4
Prithvi-WhiteHat <76204104+Prithvi-WhiteHat@users.noreply.github.com>
2021-01-05 15:33:58 +0530

    Add files via upload

---
+++ library1.js
@@ -0,0 +1,41 @@
+function istouch( object1, object2){
+
+    if ( object1.x - object2.x < object2.width/2 + object1.width/2 
+      && object2.x - object1.x < object2.width/2 + object1.width/2 
+      && object1.y - object2.y < object2.height/2 + object1.height/2 
+      && object2.y - object1.y < object2.height/2 + object1.height/2 )
+      {
+   object1 .shapeColor = "red";
+  object2.shapeColor = "red";
+  }
+  else {
+    object1 .shapeColor = "green";
+    object2.shapeColor = "green";
+  }
+}
+
+
+
+
+function bounceOff (object1,object2){
+
+    if ( object1.x - object2.x < object2.width/2 + object1.width/2 
+        && object2.x - object1.x < object2.width/2 + object1.width/2 
+      )
+        {
+     //neglate velocity 
+     object1.velocityX=-1*object1.velocityX
+    }
+    else if ( object1.y- object2.x < object2.height/2 + object1.height/2 
+        && object2.y - object1.y < object2.height/2 + object1.height/2 
+      ) {
+    
+        object1.velocityY=-1*object1.velocityY
+    }
+
+  }
+
+
+
+
+
